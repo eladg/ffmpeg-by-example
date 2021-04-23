6c56eacb7c599fe2d4427074d7df350c8a2c8542
eladg <1115533+eladg@users.noreply.github.com>
2021-04-23 05:13:00 -0700

    init serverless

---
+++ api/new_post.py
@@ -0,0 +1,20 @@
+import json
+
+def write_new_post_file(options):
+  pass
+
+def validate(options):
+  pass
+
+def run(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
