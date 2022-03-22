f5b030a858e6865ea252e428cee12adab269aa41
Elad Gariany <1115533+eladg@users.noreply.github.com>
2022-03-22 03:42:54 -0700

    update example player data

--- api/post_new_example/index.js
+++ api/post_new_example/index.js
@@ -99,7 +99,9 @@ const handler = async (event, context) => {
     thumbnail_url: body.thumbnail_url || null,
     terminal_command: body.terminal_command,
     example_type: body.example_type,
-    example_player_data: body.example_player_data,
+    example_player_data: [ 
+      body.example_player_data 
+    ],
     views: 0,
     likes: 0,    
   }
