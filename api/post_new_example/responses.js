e7a57b0e49d2b2d947f93b950506b18e76cab857
Elad Gariany <1115533+eladg@users.noreply.github.com>
2022-04-17 16:49:43 -0700

    New Contribute VueJS SPA 🎉
    + vendor vuejs
    + vendor markedjs
    + recreate the contribute page with 5 steps
    + update design
    + cleanup css

--- api/post_new_example/responses.js
+++ api/post_new_example/responses.js
@@ -1,27 +1,56 @@
+const globals = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json"
+  }
+}
+
 var errorResponse = (error) => {
-  return { statusCode: 500, body: error.toString() }
+  return { 
+    ...globals,
+    statusCode: 500, 
+    body: JSON.stringify({
+      message: error.toString() 
+    }) 
+  }
 }
 
 var failedValidateInputResponse = (error) => {
-  return { statusCode: 401, body: error.toString() }
+  return { 
+    ...globals,
+    statusCode: 401, 
+    body: JSON.stringify({
+      message: error.toString() 
+    }) 
+  }
 }
 
 var failedValidateDataResponse = (error) => {
-  return { statusCode: 403, body: error.toString() }
+  return { 
+    ...globals,
+    statusCode: 403, 
+    body: JSON.stringify({
+      message: error.toString() 
+    }) 
+  }
 }
 
 var failedRequestToGithub = (error) => {
-  return { statusCode: 403, body: error.toString() }
+  return { 
+    ...globals,
+    statusCode: 403, 
+    body: JSON.stringify({
+      message: error.toString() 
+    }) 
+  }
 }
 
 var succsess = (msg) => {
   return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello from API (${msg})` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    ...globals,
+    statusCode: 200,
+    body: JSON.stringify({ message: `${msg}` }),
+  }
 }
 
 module.exports = { 
